7a19365e2801fc5f4ed6dec43152d6576096eb1a
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-20 10:04:37 +0100

    Refactor validators

--- server/middleware/board/boardValidators.js
+++ server/middleware/board/boardValidators.js
@@ -2,30 +2,13 @@ const { check } = require('express-validator');
 const Board = require('../../models/boardModel');
 
 // Validation for title and columns
-const addBoardValidator = [
-	check('name').not().isEmpty().withMessage('Name cannot be empty'),
-	check('columns').not().isEmpty().withMessage('Columns cannot be empty'),
-
-	// check if board exists
-	check('name').custom(async (name, { req }) => {
-		const board = await Board.findOne({
-			name,
-			user: req.user.id,
-		});
-		if (board) {
-			throw new Error('Board already exists');
-		}
-	}),
-];
-
-// Update board validation
-const updateBoardValidator = [
+const boardValidator = [
 	check('name').not().isEmpty().withMessage('Name cannot be empty'),
 	check('columns').not().isEmpty().withMessage('Columns cannot be empty'),
 ];
 
 // Add Column validation
-const addColumnValidator = [
+const columnValidator = [
 	check('name').not().isEmpty().withMessage('Name cannot be empty'),
 	// length of name should be less than 10
 	check('name')
@@ -34,7 +17,6 @@ const addColumnValidator = [
 ];
 
 module.exports = {
-	addBoardValidator,
-	updateBoardValidator,
-	addColumnValidator,
+	boardValidator,
+	columnValidator,
 };
