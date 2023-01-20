269ab1ecea05880d679a1996ac9ae45aa6dfaf9a
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-20 17:10:30 +0100

    Setup gulpfile

---
+++ client/gulpfile.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import dartSass from 'sass';
+const sass = gulpSass(dartSass);
+
+export function buildStyles() {
+	return gulp
+		.src('./src/styles/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: ['last 2 versions'],
+				cascade: false,
+			})
+		)
+		.pipe(gulp.dest('./src/styles/css'));
+}
+
+export function watchTask() {
+	gulp.watch(['./src/styles/scss/**/*.scss', './src/**/*.jsx'], buildStyles);
+}
+
+gulp.task('default', gulp.series(buildStyles, watchTask));
