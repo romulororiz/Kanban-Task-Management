7de03f02075c410e9ee7a4f8309777128f2314e9
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-21 17:04:41 +0100

    Define base styles and variables

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '@styles/scss/index.scss';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import '@styles/scss/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
