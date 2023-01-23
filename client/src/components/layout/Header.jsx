580947bca071d8a6e947ce6653a90c40949228cb
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-23 18:16:14 +0100

    style based on showSidebar

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -3,11 +3,19 @@ import Add from '@assets/dashboard/icon-add-task-mobile.svg';
 import LogoDark from '@assets/dashboard/logo-dark.svg';
 import '@styles/scss/layout/Header.scss';
 
-const Header = () => {
+const Header = ({ showSidebar }) => {
 	return (
 		<div className='kanban__header'>
-			<div className='kanban__header-container'>
-				<div className='kanban__header-logo'>
+			<div
+				className={`kanban__header-container ${
+					!showSidebar && 'kanban__header-container_no-sidebar'
+				}`}
+			>
+				<div
+					className={`kanban__header-logo ${
+						!showSidebar && 'kanban__header-logo_no-sidebar'
+					}`}
+				>
 					<img src={LogoDark} alt='logo dark' />
 				</div>
 				<div className='kanban__header-board_name'>Board Name</div>
