2b7040f539946fb3e568aa04314b359c6dc1e209
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-23 10:14:35 +0100

    Define slice and service for getting/adding/updating and deleting a board

---
+++ client/src/features/boards/boardService.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const API_URL = '/api/boards';
+
+// Get all boards
+const getBoards = async () => {
+	const response = await axios.get(API_URL);
+	return response.data;
+};
+
+// Create new board
+const createBoard = async boardData => {
+	const response = await axios.post(API_URL, boardData);
+	return response.data;
+};
+
+// Get board by id
+const getBoardById = async boardId => {
+	const response = await axios.get(`${API_URL}/${boardId}`);
+	return response.data;
+};
+
+// Update board
+const updateBoard = async boardData => {
+	const response = await axios.put(`${API_URL}/${boardData._id}`, boardData);
+	return response.data;
+};
+
+// Delete board
+const deleteBoard = async boardId => {
+	const response = await axios.delete(`${API_URL}/${boardId}`);
+	return response.data;
+};
+
+const boardService = {
+	getBoards,
+	createBoard,
+	getBoardById,
+	updateBoard,
+	deleteBoard,
+};
+
+export default boardService;
---
+++ client/src/features/boards/boardSlice.js
@@ -0,0 +1,179 @@
+// create a slice of state for the board
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import boardService from './boardService';
+import Cookies from 'js-cookie';
+
+const initialState = {
+	boards: [],
+	board: {},
+	isLoading: false,
+	isError: false,
+	isSuccess: false,
+	errors: [],
+};
+
+// Get all boards
+export const getBoards = createAsyncThunk(
+	'board/getBoards',
+	async (_, { rejectWithValue }) => {
+		try {
+			const response = await boardService.getBoards();
+			return response;
+		} catch (error) {
+			const errors = error.response.data.errors;
+			return rejectWithValue(errors);
+		}
+	}
+);
+
+// Create new board
+export const createBoard = createAsyncThunk(
+	'board/createBoard',
+	async (boardData, { rejectWithValue }) => {
+		try {
+			const response = await boardService.createBoard(boardData);
+			return response;
+		} catch (error) {
+			const errors = error.response.data.errors;
+			return rejectWithValue(errors);
+		}
+	}
+);
+
+// Get board by id
+export const getBoardById = createAsyncThunk(
+	'board/getBoardById',
+	async (boardId, { rejectWithValue }) => {
+		try {
+			const response = await boardService.getBoardById(boardId);
+			return response;
+		} catch (error) {
+			const errors = error.response.data.errors;
+			return rejectWithValue(errors);
+		}
+	}
+);
+
+// Update board
+export const updateBoard = createAsyncThunk(
+	'board/updateBoard',
+	async (boardData, { rejectWithValue }) => {
+		try {
+			const response = await boardService.updateBoard(boardData);
+			return response;
+		} catch (error) {
+			const errors = error.response.data.errors;
+			return rejectWithValue(errors);
+		}
+	}
+);
+
+// Delete board
+export const deleteBoard = createAsyncThunk(
+	'board/deleteBoard',
+	async (boardId, { rejectWithValue }) => {
+		try {
+			const response = await boardService.deleteBoard(boardId);
+			return response;
+		} catch (error) {
+			const errors = error.response.data.errors;
+			return rejectWithValue(errors);
+		}
+	}
+);
+
+// Board Slice Reducer
+const boardSlice = createSlice({
+	name: 'board',
+	initialState,
+	reducers: {
+		clearErrors: state => {
+			state.errors = [];
+		},
+	},
+	extraReducers: builder => {
+		builder
+			.addCase(getBoards.pending, state => {
+				state.isLoading = true;
+			})
+			.addCase(getBoards.fulfilled, (state, { payload }) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.boards = payload;
+			})
+			.addCase(getBoards.rejected, (state, { payload }) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.errors = payload;
+			})
+			.addCase(createBoard.pending, state => {
+				state.isLoading = true;
+			})
+			.addCase(createBoard.fulfilled, (state, { payload }) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				// create new board and push it to the boards array
+				const newBoard = {
+					_id: payload._id,
+					name: payload.name,
+				};
+				state.boards.push(newBoard);
+			})
+			.addCase(createBoard.rejected, (state, { payload }) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.errors = payload;
+			})
+			.addCase(getBoardById.pending, state => {
+				state.isLoading = true;
+			})
+			.addCase(getBoardById.fulfilled, (state, { payload }) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.board = payload;
+			})
+			.addCase(getBoardById.rejected, (state, { payload }) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.errors = payload;
+			})
+			.addCase(updateBoard.pending, state => {
+				state.isLoading = true;
+			})
+			.addCase(updateBoard.fulfilled, (state, { payload }) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				// find the board in the boards array and update it
+				const index = state.boards.findIndex(
+					board => board._id === payload._id
+				);
+				state.boards[index].name = payload.name;
+			})
+			.addCase(updateBoard.rejected, (state, { payload }) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.errors = payload;
+			})
+			.addCase(deleteBoard.pending, state => {
+				state.isLoading = true;
+			})
+			.addCase(deleteBoard.fulfilled, (state, { payload }) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				// find the board in the boards array and remove it
+				const index = state.boards.findIndex(
+					board => board._id === payload._id
+				);
+				state.boards.splice(index, 1);
+			})
+			.addCase(deleteBoard.rejected, (state, { payload }) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.errors = payload;
+			});
+	},
+});
+
+export const { clearErrors } = boardSlice.actions;
+
+export default boardSlice.reducer;
