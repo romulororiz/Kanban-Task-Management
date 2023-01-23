afc9d68cb622679c6d5dcea209eb2109962ed558
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-23 10:13:49 +0100

    add boardReducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,11 @@
 // create redux toolkit store
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '@features/auth/authSlice';
+import boardReducer from '@features/boards/boardSlice';
 
 export default configureStore({
 	reducer: {
 		auth: authReducer,
+		board: boardReducer,
 	},
 });
