d8508b09e945b3e0f108b93d06966fd5012ca4be
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-24 17:02:00 +0100

    Add Spinner Component

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import '@styles/scss/Spinner.scss';
+
+const Spinner = () => {
+	return (
+		<div className='loader'>
+			<div className='spinner'></div>
+		</div>
+	);
+};
+
+export default Spinner;
