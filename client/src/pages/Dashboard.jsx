2ffa6c6049d8ccfdda02dada1fe87a6e3c25ff7a
Romulo Roriz <romulonmroriz@gmail.com>
2023-01-24 12:26:46 +0100

    Style dashboard general styles

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
+import { useSelector } from 'react-redux';
+import Column from '@components/board/Column';
+import '@styles/scss/boards/Dashboard.scss';
+import { useEffect } from 'react';
+
 const Dashboard = () => {
+	// const { columns } = useSelector(state => state.column);
+
+	// get columns for this board
+	// useEffect(() => {
+	// 	dispatch(getColumns(boardId));
+	// }, [dispatch, boardId]);
+
 	return (
-		<div>
-			<h1>Boards</h1>
+		<div className='kanban__dashboard-board'>
+			<Column name='Todo' />
+			<Column name='Todo' />
+			<Column name='Todo' />
+			<Column name='Todo' />
+			<Column name='Todo' />
+			<Column name='Todo' />
+			<Column name='Todo' />
+			<Column name='Todo' />
 		</div>
 	);
 };
